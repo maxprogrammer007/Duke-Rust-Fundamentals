d542c9b825417e366c450593e4875fa3bc7658de
Abhinav Shukla <67401627+maxprogrammer007@users.noreply.github.com>
2024-04-07 14:23:59 +0530

    controlflow lab

--- week_2/src/controlflow.rs
+++ week_2/src/controlflow.rs
@@ -1,19 +1,28 @@
 fn main() {
-	let proceed = true;
-	if proceed{
-		println!("Proceeding");
-	}
-	else {
-		println!("Not Proceeding")
-	}
+    let proceed = false;
+    if proceed {
+        println!("Proceeding");
+    } else {
+        println!("Not proceeding");
+    }
 
-	let height = 190;
-	if height > 180 {
-		println!("Tall");
-	}
-	else if height >!70 {
-		println!("Average");
-	} else{
-		println!("Short");
-	}
+    let height = 190;
+    if height < 180 {
+        println!("Tall");
+    } else if height > 170 {
+        println!("Average");
+    } else {
+        println!("Short");
+    }
+    let age = 19;
+    if age < 12{
+        println!("Child");
+    }
+    else if age > 13 && age < 19{
+        println!("Teenager");
+    } 
+    else{
+        println!("Adult");
+    }
+
 }
