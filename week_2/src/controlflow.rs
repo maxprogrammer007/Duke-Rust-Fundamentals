c9c7d375c31bff985c08678d4cc614983681e819
Abhinav Shukla <67401627+maxprogrammer007@users.noreply.github.com>
2024-04-07 14:13:06 +0530

    controlflow

---
+++ week_2/src/controlflow.rs
@@ -0,0 +1,19 @@
+fn main() {
+	let proceed = true;
+	if proceed{
+		println!("Proceeding");
+	}
+	else {
+		println!("Not Proceeding")
+	}
+
+	let height = 190;
+	if height > 180 {
+		println!("Tall");
+	}
+	else if height >!70 {
+		println!("Average");
+	} else{
+		println!("Short");
+	}
+}
